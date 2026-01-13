d05431253dd761f398158405fcf432a67779d20e
Obianuju-Sunday <obianujusunday43@gmail.com>
2026-01-13 16:45:35 +0100

    auth middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,50 @@
+
+const jwt = require('jsonwebtoken');
+const AuthError = require('../errors/AuthError');
+
+
+const protect = (req, res, next) => {
+
+    try {
+        let token;
+
+        // Check for token in Authorization header
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1];
+        }
+
+        if (!token) {
+            return next(new AuthError('Access denied. No token provided.')); 
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = {
+            id: decoded.id,
+            email: decoded.email
+        };
+
+        return next();
+
+    } catch (err) {
+
+        if (err.name === 'JsonWebTokenError') {
+            return next(new AuthError('Invalid or expired token.'));
+        }
+
+        if (err.name === 'TokenExpiredError') {
+            return next(new AuthError('Token expired.'));
+        }
+
+        if(err.name === 'NotBeforeError'){
+            return next(new AuthError('Token not active yet.'))
+        }
+
+        next(err);
+    }
+
+}
+
+module.exports = {
+    protect
+};
--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,16 @@
 const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controller/AuthController');
+const { protect } = require('../middleware/authMiddleware')
 
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/test-protected', protect, (req, res) => {
+    res.json({ 
+        message: 'You have accessed a protected route', 
+        user: req.user 
+    });
+})
 
 module.exports = router;
